c5a2a6789f241f6761cee6aff27f9078aa163884
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-18 03:15:54 +0200

    Add ZStack and VStack

--- SwiftUI_VStak&ZStak/ContentView.swift
+++ SwiftUI_VStak&ZStak/ContentView.swift
@@ -10,7 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack{
+                Image("Cat")
+                    .resizable()
+                    .frame(width: 300, height: 300, alignment: .center)
+                    .blendMode(.hardLight)
+                
+                Text("🥰 Meow! 🥰")
+                    .font(Font.system(.largeTitle, design: .monospaced))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .shadow(color: Color.black, radius: 10, x: 10, y: 10)
+                .rotationEffect(Angle(degrees: -10))
+            }
+            Spacer()
+            .layoutPriority(1)
+        }
+        .background(Color.myFavoriteGreen)
+        .edgesIgnoringSafeArea(.all)
+
+        
     }
 }
 
@@ -18,4 +38,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+// Everyone's favorite color
+extension Color {
+  static let myFavoriteGreen = Color(red: 21/255, green: 132/255, blue: 67/255)
 }
